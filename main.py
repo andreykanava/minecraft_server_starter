2ffa2a1be2238613e5afb8e32b1d23d4b975c768
andreykanava <66001105+andreykanava@users.noreply.github.com>
2022-08-26 12:16:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from python_aternos import Client
+from discord.ext import commands
+
+discord = commands.Bot(command_prefix='!')
+aternos = Client.from_credentials('Weyther', 'Qawe4r%tyu*i')
+
+@discord.command(aliases=["startserver", "ss", "start"])
+async def start_server(ctx):
+    try:
+        servs = aternos.list_servers()
+        myserv = servs[0]
+        myserv.start()
+        await  ctx.channel.send(content=":white_check_mark:Запускаю! Почекайте декілька хвилин:white_check_mark: ")
+    except:
+        await  ctx.channel.send(content=":white_check_mark:Сервер вже запущений!:white_check_mark: ")
+
+discord.run("MTAxMjA1NDAzMTk0Mzg3MjU1Mw.G8uP5Z.WYnj13E_nJFVVrUFpRrO9tft12VaaJf7FpWtuE")
